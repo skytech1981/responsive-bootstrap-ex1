fe3995fc2cb85995d74ae6126a4753ed4af84f2d
sali itzhaki <sali1943@hotmail.com>
2020-07-12 21:06:33 +0300

    adding ajax function for buttons

--- script.js
+++ script.js
@@ -4,5 +4,21 @@ $(document).ready(function() {
     $('#btnToggle').click(function () {
       $('#collapsibleNavbar').toggle(500);
     });
-  });
-  
+
+   
+    $("#index").click(function() {
+     $("#main").load("index.html");
+       })
+
+    $("#about").click(function() {
+     $("#main").load("about.html");
+       })
+
+    $("#contact").click(function() {
+     $("#main").load("contact.html");
+          })
+      
+        });
+   
+
+
